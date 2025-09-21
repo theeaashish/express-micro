f94b4b1e6d608e434e5452eacafa6fc89cf51665
Aashish Joshi <theeaashish@gmail.com>
2025-09-21 09:50:36 +0000

    add error handling and db create user

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import prisma from "../config/db.config.ts";
 import vine, { errors } from "@vinejs/vine";
 import { registerSchema } from "../validations/authValidation.ts";
 import type { Request, Response } from "express";
+import bcrypt from "bcryptjs";
 
 class AuthController {
   static async register(req: Request, res: Response) {
@@ -9,11 +10,45 @@ class AuthController {
       const body = await req.body;
       const validator = vine.compile(registerSchema);
       const payload = await validator.validate(body);
-      return res.json({ payload });
+
+      // check if email exists
+      const findUser = await prisma.users.findUnique({
+        where: {
+          email: payload.email,
+        },
+      });
+
+      if (findUser) {
+        return res.status(400).json({
+          erros: {
+            email: "Email already exists",
+          },
+        });
+      }
+
+      // encrypt password
+      const salt = await bcrypt.genSalt(10);
+      payload.password = await bcrypt.hash(payload.password, salt);
+
+      // store user in db
+      const user = await prisma.users.create({
+        data: payload,
+      });
+
+      return res.json({
+        status: 200,
+        message: "User Created Successfully",
+        user,
+      });
     } catch (error) {
       if (error instanceof errors.E_VALIDATION_ERROR) {
         return res.status(400).json({
           errors: error.messages,
+        });
+      } else {
+        return res.status(500).json({
+          message: "Something went wrong",
+          error,
         });
       }
     }
